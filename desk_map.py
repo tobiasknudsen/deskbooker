ec35c4eb92bb7a69a17c5cb356b7fe203187f4e3
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2022-04-21 15:22:13 +0200

    Add desk map to relate desk id to actual desks in Growth section

---
+++ desk_map.py
@@ -0,0 +1,22 @@
+#Format of GROWTH_DESKS is Desk number: desk_id
+GROWTH_DESKS = {
+    "Desk 1": 77876,
+    "Desk 2": 77877,
+    "Desk 3": 77878,
+    "Desk 4": 77879,
+    "Desk 5": 77880,
+    "Desk 6": 77881,
+    "Desk 7": 77882,
+    "Desk 8": 77883,
+    "Desk 9": 77884,
+    "Desk 10": None,
+    "Desk 11": 77976,
+    "Desk 12": 77977,
+    "Desk 13": 77978,
+    "Desk 14": 77979,
+    "Desk 15": 78201,
+    "Desk 16": 78202,
+    "Desk 17": 78203,
+    "Desk 18": 78204
+}
+
