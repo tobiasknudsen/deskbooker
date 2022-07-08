43a01eb8ae5fd6de7aa9a5c995e7dcd77af5112f
Tobias Knudsen <tobias.hartvedt.knudsen@gmail.com>
2022-07-08 09:19:28 +0200

    Put base urls in constant variables

--- deskbooker/deskbird_client.py
+++ deskbooker/deskbird_client.py
@@ -13,6 +13,8 @@ class DeskbirdClient:
     resource_id = None
     zone_item_id = None
     workspace_id = None
+    API_BASE_URL = "https://app.deskbird.com/api/v1.1"
+    APP_BASE_URL = "https://web.deskbird.app/api/v1.1"
 
     def __init__(
         self,
@@ -31,7 +33,7 @@ class DeskbirdClient:
 
     def set_zone_item_id(self, zone_name, desk_id):
         url = (
-            f"https://app.deskbird.com/api/v1.1/internalWorkspaces/"
+            f"{self.API_BASE_URL}/internalWorkspaces/"
             f"{self.workspace_id}/zones?internal"
         )
         headers = {
@@ -48,7 +50,7 @@ class DeskbirdClient:
         raise KeyError(f"zone_name: {zone_name} does not exists")
 
     def book_desk(self, date):
-        url = "https://web.deskbird.app/api/v1.1/user/bookings"
+        url = f"{self.APP_BASE_URL}/user/bookings"
         if not self.zone_item_id:
             raise Exception("ZONE_ITEM_ID missing from environment")
         body = {
@@ -73,10 +75,7 @@ class DeskbirdClient:
         return requests.post(url, headers=headers, data=json.dumps(body))
 
     def get_bookings(self, limit=10):
-        url = (
-            "https://app.deskbird.com/api/v1.1/user/bookings"
-            f"?upcoming=true&skip=0&limit={limit}"
-        )
+        url = f"{self.API_BASE_URL}/user/bookings?upcoming=true&skip=0&limit={limit}"
         headers = {
             "Authorization": f"Bearer {self.access_token}",
         }
@@ -84,10 +83,7 @@ class DeskbirdClient:
         return requests.get(url, headers=headers)
 
     def checkin(self):
-        url = (
-            f"https://app.deskbird.com/api/v1.1/workspaces/"
-            f"{self.workspace_id}/checkIn"
-        )
+        url = f"{self.API_BASE_URL}/workspaces/{self.workspace_id}/checkIn"
         body = {
             "isInternal": True,
             "resourceId": self.resource_id,
