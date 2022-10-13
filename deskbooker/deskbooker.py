974ef2edb9aba7f77eb385621e5123b1e6840467
Tobias Knudsen <tobias.hartvedt.knudsen@gmail.com>
2022-10-13 08:24:23 +0200

    Small fix in deskbird response

--- deskbooker/deskbooker.py
+++ deskbooker/deskbooker.py
@@ -50,7 +50,7 @@ def main():
                     datetime.fromtimestamp(
                         int(booking["bookingStartTime"] / 1000)
                     ).date(),
-                    booking["resource"]["groupName"],
+                    booking["workspace"]["name"],
                     f"{booking['resource']['name']} {booking['zoneItemName']}",
                     "✅" if booking["checkInStatus"] == "checkedIn" else "❌",
                 ]
