386c3e743066ed9213385e7a7a9d9cad7d3020b5
Tobias Knudsen <tobias.hartvedt.knudsen@gmail.com>
2022-11-17 08:58:26 +0100

    Refactor deskbooker.py with separat function functions

--- deskbooker/deskbooker.py
+++ deskbooker/deskbooker.py
@@ -33,86 +33,95 @@ arg_parser.add_argument("-d", "--desk", dest="desk_number", help="Desk number")
 arg_parser.add_argument("-z", "--zone", dest="zone", help="Set zone")
 
 
+def checkin(args):
+    db_client.checkin()
+
+
+def cancel(args):
+    db_client.cancel_booking()
+
+
+def bookings(args):
+    bookings = json.loads(db_client.get_bookings(limit=30).text)
+    bookings_table = PrettyTable(["Date", "Zone", "Desk", "Check-in"])
+
+    for booking in bookings["results"]:
+        booking_list = [
+            datetime.fromtimestamp(int(booking["bookingStartTime"] / 1000)).date(),
+            booking["workspace"]["name"],
+            f"{booking['resource']['name']} {booking['zoneItemName']}",
+            "✅" if booking["checkInStatus"] == "checkedIn" else "❌",
+        ]
+        bookings_table.add_row(booking_list)
+    print(bookings_table)
+
+
+def book(args):
+    try:
+        from_date = dateutil.parser.parse(args.from_date)
+    except dateutil.parser._parser.ParserError:
+        arg_parser.error(f"{args.from_date} is not a valid date format")
+    try:
+        to_date = dateutil.parser.parse(args.to_date)
+    except dateutil.parser._parser.ParserError:
+        arg_parser.error(f"{args.to_date} is not a valid date format")
+
+    if (args.zone is None) != (args.desk_number is None):
+        print("either of the following arguments are required: -z/--zone, -d/--desk")
+        return
+    if args.zone is not None and args.desk_number is not None:
+        try:
+            db_client.set_zone_item_id(zone_name=args.zone, desk_id=args.desk_number)
+        except KeyError as e:
+            arg_parser.error(e)
+    response = db_client.book_desk(from_date=from_date, to_date=to_date)
+    data_response = json.loads(response.text)
+    bookings = [
+        booking["booking"] for booking in data_response["data"]["successfulBookings"]
+    ] + data_response["data"]["failedBookings"]
+    bookings = sorted(bookings, key=lambda booking: booking["bookingStartTime"])
+    for booking in bookings:
+        if "errorMessage" in booking:
+            print(
+                " | ".join(
+                    [
+                        "❌",
+                        str(
+                            datetime.fromtimestamp(
+                                booking["bookingStartTime"] / 1000
+                            ).date()
+                        ),
+                        str(booking["errorMessage"]),
+                    ]
+                )
+            )
+        else:
+            print(
+                " | ".join(
+                    [
+                        "✅",
+                        str(
+                            datetime.fromtimestamp(
+                                booking["bookingStartTime"] / 1000
+                            ).date()
+                        ),
+                        str(booking["bookingStatus"]),
+                    ]
+                )
+            )
+
+
 def main():
     try:
         args = arg_parser.parse_args()
         if args.function_name == "checkin":
-            db_client.checkin()
+            checkin(args)
         elif args.function_name == "cancel":
-            # TODO Add functionality to choose which day to cancel
-            db_client.cancel_booking()
+            cancel(args)
         elif args.function_name == "bookings":
-            bookings = json.loads(db_client.get_bookings(limit=30).text)
-            bookings_table = PrettyTable(["Date", "Zone", "Desk", "Check-in"])
-
-            for booking in bookings["results"]:
-                booking_list = [
-                    datetime.fromtimestamp(
-                        int(booking["bookingStartTime"] / 1000)
-                    ).date(),
-                    booking["workspace"]["name"],
-                    f"{booking['resource']['name']} {booking['zoneItemName']}",
-                    "✅" if booking["checkInStatus"] == "checkedIn" else "❌",
-                ]
-                bookings_table.add_row(booking_list)
-            print(bookings_table)
+            bookings(args)
         elif args.function_name == "book":
-            if args.from_date is None or args.to_date is None:
-                arg_parser.error("book requires --from and --to")
-            try:
-                from_date = dateutil.parser.parse(args.from_date)
-            except dateutil.parser._parser.ParserError:
-                arg_parser.error(f"{args.from_date} is not a valid date format")
-            try:
-                to_date = dateutil.parser.parse(args.to_date)
-            except dateutil.parser._parser.ParserError:
-                arg_parser.error(f"{args.to_date} is not a valid date format")
-            if (args.zone is None) != (args.desk_number is None):
-                print("Both zone and desk_number is not present.")
-                return
-            if args.zone is not None and args.desk_number is not None:
-                try:
-                    db_client.set_zone_item_id(
-                        zone_name=args.zone, desk_id=args.desk_number
-                    )
-                except KeyError as e:
-                    arg_parser.error(e)
-            response = db_client.book_desk(from_date=from_date, to_date=to_date)
-            data_response = json.loads(response.text)
-            bookings = [
-                booking["booking"]
-                for booking in data_response["data"]["successfulBookings"]
-            ] + data_response["data"]["failedBookings"]
-            bookings = sorted(bookings, key=lambda booking: booking["bookingStartTime"])
-            for booking in bookings:
-                if "errorCode" in booking:
-                    print(
-                        " | ".join(
-                            [
-                                "❌",
-                                str(
-                                    datetime.fromtimestamp(
-                                        booking["bookingStartTime"] / 1000
-                                    ).date()
-                                ),
-                                str(booking["errorCode"]),
-                            ]
-                        )
-                    )
-                else:
-                    print(
-                        " | ".join(
-                            [
-                                "✅",
-                                str(
-                                    datetime.fromtimestamp(
-                                        booking["bookingStartTime"] / 1000
-                                    ).date()
-                                ),
-                                str(booking["bookingStatus"]),
-                            ]
-                        )
-                    )
+            book(args)
         return 0
     except KeyboardInterrupt:
         print("Stopping...")
